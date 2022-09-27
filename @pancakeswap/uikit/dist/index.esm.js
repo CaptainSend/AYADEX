9e005db25406e4dcb8895fc5af6c5a40a7f6b7a1
danieljohansson <danieljohansson@atlassian.com>
2022-09-27 11:42:03 +0200

    bubble

--- @pancakeswap/uikit/dist/index.esm.js
+++ @pancakeswap/uikit/dist/index.esm.js
@@ -2775,8 +2775,8 @@ var lightColors = __assign(__assign(__assign({}, baseColors), additionalColors),
         gold: "linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), additionalColors), { secondary: "#ff6497", background: "#1c1b21", backgroundDisabled: "#3c3742", backgroundAlt: "#1c1b21", backgroundAlt2: "#1c1b21", cardBorder: "#383241", contrast: "#FFFFFF", dropdown: "#1E1D20", dropdownDeep: "#100C18", invertedContrast: "#191326", input: "#372F47", inputSecondary: "#262130", primaryDark: "#ff6497", tertiary: "#353547", text: "#ff6497", textDisabled: "#666171", textSubtle: "#B8ADD2", disabled: "#524B63", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #ff6497 0%, #FFFFFF 100%)",
-        inverseBubblegum: "linear-gradient(139.73deg, #FFFFFF 0%, #ff6497 100%)",
+        bubblegum: "linear-gradient(139.73deg, #FFFFFF 0%, #ff6497 100%)",
+        inverseBubblegum: "linear-gradient(139.73deg, #ff6497 0%, #FFFFFF 100%)",
         cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
         blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
         violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
